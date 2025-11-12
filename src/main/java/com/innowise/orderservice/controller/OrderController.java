6f79a2d5dd3b065684545eac62f3f35f749b3a6b
Nestle-afk <nastassya04@gmail.com>
2025-11-12 14:07:24 +0300

    add: controller, GlobalExceptionHandler

---
+++ src/main/java/com/innowise/orderservice/controller/OrderController.java
@@ -0,0 +1,50 @@
+package com.innowise.orderservice.controller;
+
+import com.innowise.orderservice.dto.OrderDto;
+import com.innowise.orderservice.model.OrderStatus;
+import com.innowise.orderservice.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDto> get(@PathVariable Long id) {
+        return ResponseEntity.ok().body(orderService.getOrder(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderDto>> getOrdersByStatuses(
+            @RequestParam List<OrderStatus> statuses) {
+        return ResponseEntity.ok(orderService.getOrdersByStatuses(statuses));
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody OrderDto dto) {
+        OrderDto created = orderService.createOrder(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderDto> updateOrder(@PathVariable Long id,
+                                                @Valid @RequestBody OrderDto dto) {
+        return ResponseEntity.ok(orderService.updateOrder(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+}
